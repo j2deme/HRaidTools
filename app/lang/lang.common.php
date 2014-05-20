71779a3e972fb9cc4f367529061b325855516b44
Jaime Delgado Meraz <j2deme@gmail.com>
2014-05-20 11:40:44 -0500

    Fixed to include a language selector on navbar

--- app/lang/lang.common.php
+++ app/lang/lang.common.php
@@ -1,10 +1,10 @@
 <?php
-$lang = new StdClass;
+$lang = array();
 /* Languages Available - First language is set as default*/
-$lang->langs = array(
+$lang['langs'] = array(
   array('suffix' => 'en','name' => 'English'),
   array('suffix' => 'es','name' => 'Español')
 );
-$lang->authorLink = 'http://github.com/j2deme';
-$lang->authorText = '@j2deme';
+$lang['authorLink'] = 'http://github.com/j2deme';
+$lang['authorText'] = '@j2deme';
 ?>
--- app/navbar.inc.php
+++ app/navbar.inc.php
@@ -1,51 +1,17 @@
 <?php
-include_once 'models/navbar.class.php';
-$navbar = array();
-$langSel = new Opt();
-$langSel->icon('fa fa-globe');
-foreach ($lang->langs as $lg) {
-  $langSel->addSub(Opt::nw()->init($lg['name'],$resourceUri)->addData('lang',$lg['suffix']));
+$langSel = array();
+$langSel['icon'] = 'fa fa-globe';
+$langSel['isDrop'] = true;
+$langSel['sub'] = array();
+foreach ($lang['langs'] as $lg) {
+  $langSel['sub'][] = array(
+    'text' => $lg['name'],
+    'url' => $resourceUri,
+    'data' => array('lang' => $lg['suffix'])
+  );
 }
 
-/***
-* Edit from here
-* See: README > Navbar configuration
-***/
-$navbar['public'] = array(
-  'left' => array(
-    Opt::nw()->init($lang->link1,'#1'),
-    Opt::nw()->init($lang->link2,'#2'),
-    Opt::nw()->init($lang->link3,'#3'),
-    Opt::nw()->init($lang->dropdown)
-      ->addSub(Opt::nw()->init($lang->google,'http://google.com'))
-      ->addSub(Opt::nw()->isDiv())
-      ->addSub(Opt::nw()->isHeader($lang->navHeader))
-      ->addSub(Opt::nw()->init($lang->facebook,'http://facebook.com')),
-  ),
-  'right' => array(
-  ),
-);
-
-$navbar['user'] = array(
-  'left' => array(
-    Opt::nw()->init($lang->link1,'#1'),
-    Opt::nw()->init($lang->link2,'#2'),
-    Opt::nw()->init($lang->link3,'#3'),
-    Opt::nw()->init($lang->dropdown)
-      ->addSub(Opt::nw()->isHeader($lang->navHeader))
-      ->addSub(Opt::nw()->init($lang->facebook,'http://facebook.com'))
-  ),
-  'right' => array(
-    Opt::nw()->init()->icon('fa fa-user')
-      ->addSub(Opt::nw()->init($lang->user,$app->urlFor('root')))
-      ->addSub(Opt::nw()->init($lang->logout,$app->urlFor('logout')))
-  ),
-);
-/***
-* DO NOT MODIFY
-* Otherwise language selector won't be loaded on the UI
-***/
-foreach ($navbar as $key => $value) {
-  $navbar[$key]['right'][] = $langSel;
+foreach ($lang['navbar'] as $key => $value) {
+  $lang['navbar'][$key]['right'][] = $langSel;
 }
 ?>
